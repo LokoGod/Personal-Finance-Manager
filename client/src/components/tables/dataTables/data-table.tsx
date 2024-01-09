70ca73703b79cd087411a4c7176fd9e4350ff928
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-01-09 12:55:06 +0530

    table UI

--- client/src/components/tables/dataTables/data-table.tsx
+++ client/src/components/tables/dataTables/data-table.tsx
@@ -32,7 +32,7 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="rounded-md border mt-5">
+    <div className="rounded-md border mt-5 bg-white">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
