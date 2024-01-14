6fa9e47babb479035bbb800bf0a5c5225be9fec8
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-01-14 11:49:25 +0530

    badge

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,37 +9,30 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-
+} from "@/components/ui/card";
 
 const Home = () => {
   return (
     <div>
       <HomeCardData />
 
-      
-
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-4">
-                  <CardHeader>
-                    <CardTitle>Overview</CardTitle>
-                  </CardHeader>
-                  <CardContent className="pl-2">
-                  <DashBarChart />
-                  </CardContent>
-                </Card>
-                <Card className="col-span-3">
-                  <CardHeader>
-                    <CardTitle>Recent Sales</CardTitle>
-                    <CardDescription>
-                      You made 265 sales this month.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    {/* <RecentSales /> */}
-                  </CardContent>
-                </Card>
-              </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <DashBarChart />
+          </CardContent>
+        </Card>
+        <Card className="col-span-3">
+          <CardHeader>
+            <CardTitle>Recent Sales</CardTitle>
+            <CardDescription>You made 265 sales this month.</CardDescription>
+          </CardHeader>
+          <CardContent>{/* <RecentSales /> */}</CardContent>
+        </Card>
+      </div>
     </div>
   );
 };
--- client/src/components/visualizations/HomeCardData.tsx
+++ client/src/components/visualizations/HomeCardData.tsx
@@ -19,7 +19,6 @@ import { LuDollarSign } from "react-icons/lu";
 const HomeCardData = () => {
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4 mb-5">
-
       <Card className="bg-white">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
@@ -28,11 +27,9 @@ const HomeCardData = () => {
         <CardContent>
           <div className="text-2xl font-bold">$45,231.89</div>
 
-          
           <div className="text-xs text-muted-foreground">
-          <Badge>+20.1%</Badge> from last month
+            <Badge>+20.1%</Badge> from last month
           </div>
-
         </CardContent>
       </Card>
 
@@ -43,9 +40,9 @@ const HomeCardData = () => {
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">+2350</div>
-          <p className="text-xs text-muted-foreground">
-            +180.1% from last month
-          </p>
+          <div className="text-xs text-muted-foreground">
+            <Badge>+180.1%</Badge> from last month
+          </div>
         </CardContent>
       </Card>
 
@@ -56,7 +53,7 @@ const HomeCardData = () => {
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">+12,234</div>
-          <p className="text-xs text-muted-foreground">+19% from last month</p>
+          <div className="text-xs text-muted-foreground"><Badge>+19%</Badge> from last month</div>
         </CardContent>
       </Card>
 
@@ -67,10 +64,9 @@ const HomeCardData = () => {
         </CardHeader>
         <CardContent>
           <div className="text-2xl font-bold">+573</div>
-          <p className="text-xs text-muted-foreground">+201 since last hour</p>
+          <div className="text-xs text-muted-foreground"><Badge>+201</Badge> since last hour</div>
         </CardContent>
       </Card>
-
     </div>
   );
 };
