23b0a9e42cdf3072c297172a8d0eadd27e0eeef5
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-01-14 16:23:31 +0530

    ui

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,12 +1,29 @@
-'use client';
+"use client";
 
-import React from 'react';
+import React from "react";
 
-import Link from 'next/link';
-import { useSelectedLayoutSegment } from 'next/navigation';
+import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
 
-import useScroll from '@/hooks/use-scroll';
-import { cn } from '@/lib/utils';
+import useScroll from "@/hooks/use-scroll";
+import { cn } from "@/lib/utils";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { RxEraser, RxDesktop, RxLink1, RxFace, RxExit } from "react-icons/rx";
 
 const Header = () => {
   const scrolled = useScroll(5);
@@ -17,9 +34,9 @@ const Header = () => {
       className={cn(
         `sticky inset-x-0 top-0 z-30 w-full transition-all border-b border-gray-200`,
         {
-          'border-b border-gray-200 bg-white/75 backdrop-blur-lg': scrolled,
-          'border-b border-gray-200 bg-white': selectedLayout,
-        },
+          "border-b border-gray-200 bg-white/75 backdrop-blur-lg": scrolled,
+          "border-b border-gray-200 bg-white": selectedLayout,
+        }
       )}
     >
       <div className="flex h-[47px] items-center justify-between px-4">
@@ -33,11 +50,47 @@ const Header = () => {
           </Link>
         </div>
 
-        <div className="hidden md:block">
-          <div className="h-8 w-8 rounded-full bg-zinc-300 flex items-center justify-center text-center">
-            <span className="font-semibold text-sm">HQ</span>
-          </div>
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="hidden md:block">
+              <div className="h-8 w-8 rounded-full bg-zinc-300 flex items-center justify-center text-center">
+                <Avatar>
+                  <AvatarImage src="https://i.pinimg.com/736x/0d/db/ed/0ddbed5b4cd7a076c7d6889cf8a76446.jpg" />
+                  <AvatarFallback>BW</AvatarFallback>
+                </Avatar>
+              </div>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <RxEraser className="mr-2 h-4 w-4" />
+                <span className="text-xs">Profile</span>
+                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <RxDesktop className="mr-2 h-4 w-4" />
+                <span className="text-xs">Billing</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <RxLink1 className="mr-2 h-4 w-4" />
+                <span className="text-xs">Settings</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <RxFace className="mr-2 h-4 w-4" />
+                <span className="text-xs">Keyboard shortcuts</span>
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <RxExit className="mr-2 h-4 w-4" />
+                <span className="text-xs">Log out</span>
+                <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
