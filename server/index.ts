cb95fba87732c48d66a9557f2c224778110c32c5
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-01-20 01:39:02 +0530

    ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,32 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import "dotenv/config";
 
-import express from 'express'
+// Import custom routes
 
-const app = express()
 
+// Instances
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// API routing
+    // app.use("/api/v1/example", exampleRouter)
+
+const port = process.env.PORT || 9000;
+
+// Spin-up server
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
