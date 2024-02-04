5a6553c00dd6eb826bf488236844cd30f2746c6c
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2024-02-04 22:06:57 +0530

    ui

---
+++ client/src/components/forms/finance/income/ServerAction.tsx
@@ -0,0 +1,14 @@
+"use server";
+
+export async function action() {
+  const response = await fetch(
+    "https://demo-api-4n3l.onrender.com/api/v1/income/",
+    { next: { revalidate: 3600 } }
+  );
+
+  if (!response.ok) {
+    throw new Error("failed to fetch data");
+  }
+  const data = await response.json();
+  return data.income;
+}
